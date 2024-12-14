f545d5a4f35b2396c6a0bcd129bdd52913af4c9d
Youssef548 <youssefahmedpvp@gmail.com>
2024-12-14 23:35:44 +0200

    refactor: check if user exits before creating new user with prisma

--- lib/repository/user.ts
+++ lib/repository/user.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prismadb";
 import { AppError } from "@/lib/errors";
+import { NextResponse } from "next/server";
 
 export async function createUser(data: {
   name: string;
@@ -7,6 +8,16 @@ export async function createUser(data: {
   hashedPassword: string;
 }) {
   try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (user) {
+      throw new AppError("User already exists", 400);
+    }
+
     return await prisma.user.create({ data });
   } catch (error: any) {
     console.error("Database Error:", error);
