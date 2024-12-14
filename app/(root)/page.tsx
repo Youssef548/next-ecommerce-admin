54bcf3f08d4b308d679865da5f95cb34530d3b66
Youssef548 <youssefahmedpvp@gmail.com>
2024-12-14 23:57:42 +0200

    feat(show the modal on the main page to create store):

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h2>Hello DashBoard</h2>;
+"use client";
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/app/hooks/useStoreModal";
+
+export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 }
