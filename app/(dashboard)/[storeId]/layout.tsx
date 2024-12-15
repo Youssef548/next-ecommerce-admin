ecf7d4eece0a631f5d3082cc6f11117fb9ec50b5
Youssef548 <youssefahmedpvp@gmail.com>
2024-12-15 22:29:32 +0200

    feat: add dashboard layout with session validation and store access control

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,44 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+import { authOptions } from "@/lib/auth";
+import prismadb from "@/lib/prismadb";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const storeId = parseInt(params.storeId);
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/auth");
+  }
+
+  const userId = parseInt(session.user.id);
+
+  if (!userId) {
+    redirect("/auth");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      This is will be a Navbar
+      {children}
+    </>
+  );
+}
