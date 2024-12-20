3acb13775e058dc6edd6f0bb85bcb9f9cb74cff0
Youssef548 <youssefahmedpvp@gmail.com>
2024-12-20 20:38:28 +0200

    feat: add routes array

---
+++ lib/routes.ts
@@ -0,0 +1,8 @@
+export const routes = (storeId: string) => [
+  {
+    href: `/${storeId}/settings`,
+    label: "Settings",
+    activePath: `/store/${storeId}/settings`,
+  },
+  // Add more routes as needed
+];
