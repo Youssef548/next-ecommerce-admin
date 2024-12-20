5059b6efc2a2b164803eb6944017f67ffba7d463
Youssef548 <youssefahmedpvp@gmail.com>
2024-12-21 00:33:06 +0200

    refactor : remove unsed error imported

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -4,7 +4,6 @@ import { authOptions } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
-import { error } from "console";
 
 export async function POST(request: Request) {
   try {
