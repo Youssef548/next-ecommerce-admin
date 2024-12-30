3b6ee3ef866989f98f90b8c6a975bbaa33c5d49e
Youssef548 <youssefahmedpvp@gmail.com>
2024-12-30 02:02:09 +0200

    refactor (middleware): make /api public routes

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,32 @@
 import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
-// Define paths that don't require authentication
+// Define public API routes that don't require authentication
+const PUBLIC_API_ROUTES = [
+  "/api",
+];
+
 
 export default withAuth(
   function middleware(req: NextRequest) {
     return NextResponse.next();
   },
+  
   {
     callbacks: {
       authorized({ req, token }) {
-        if (
-          req.nextUrl.pathname.startsWith("/api/register") ||
-          req.nextUrl.pathname.startsWith("/api/login")
-        )
-          return true;
+        // Check if the route is a public API route
+        const isPublicApiRoute = PUBLIC_API_ROUTES.some(route =>
+          req.nextUrl.pathname.startsWith(route)
+        );
+
+        console.log("isPublicApiRoute", isPublicApiRoute);
+
+        if (isPublicApiRoute) {
+          return true; // If the route starts with "/api/path", it's allowed
+        }
+        
+        // Require authentication for other routes
         if (!token) return false;
         return true;
       },
@@ -29,7 +41,5 @@ export const config = {
   matcher: [
     // Match all paths except static files and Next.js internals
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/api/(.*)",
   ],
 };
