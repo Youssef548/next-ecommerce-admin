485b73d578c006b00719915fa58cef370ce7e419
Youssef548 <youssefahmedpvp@gmail.com>
2025-01-04 23:30:13 +0200

    feat: colors page with components

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ColorColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ColorsClientProps {
+  colors: ColorColumn[];
+}
+
+export const ColorClient = ({ colors }: ColorsClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${colors.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={colors} searchKey="name" />
+      <Heading title="API" description="API calls for Colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  );
+};
