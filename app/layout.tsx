a8071ccfa78c12d0888834fba3773b9ddef38a21
Youssef548 <youssefahmedpvp@gmail.com>
2025-08-03 22:58:59 +0300

    feat: integrate theme provider into layout
    - Add ThemeProvider component to manage theme settings
    - Wrap existing AuthContext and other providers within ThemeProvider
    - Update layout to suppress hydration warnings for better SSR handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import AuthContext from "./context/AuthContext";
 import ToasterContext from "./context/ToasterContext";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
-        <AuthContext>
-          <ModalProvider />
-          <ToasterContext />
-          {children}
-        </AuthContext>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthContext>
+            <ToasterContext />
+            <ModalProvider />
+            {children}
+          </AuthContext>
+        </ThemeProvider>
       </body>
     </html>
   );
